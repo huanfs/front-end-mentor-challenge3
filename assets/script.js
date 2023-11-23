5484599ac0096c9bd5b910b349c7402376fa5df7
Ruan Figuereido <huanfiguereido@gmail.com>
2023-11-23 17:28:56 -0300

    add: adition of readme.md and last costomizations for layouts mobile and desktop, add a errMessage to display errors and conclusion of script

--- assets/script.js
+++ assets/script.js
@@ -1,69 +1,32 @@
-//VARIÁVEIS GLOBAIS (ELEMENTOS DO DOM)
-let label = document.querySelectorAll("label");
-let inputs = document.getElementsByClassName("data-in");
-let inputGround = document.getElementsByClassName("input");
-let msg = document.getElementsByClassName("errmsg");
-let btn = document.querySelector("img");
-let outputs = document.getElementsByClassName("data");
-//ATRIBUIÇÃO DOS EVENTOS
-btn.addEventListener("click", CalcAge);
-inputs[0].addEventListener("blur", dados);
-inputs[1].addEventListener("blur", dados);
-inputs[2].addEventListener("blur", dados);
-//FUNÇÃO DE TRATAMENTO DE DADOS
-function dados(){
-    let erro="This field is required";
-    if(inputs[0].value==''){
-        inputs[0].style.border="1px solid #ec0f0f";
-        msg[0].innerHTML=erro;
-        label[0].style.color="#ec0f0f";
+let inputs = document.querySelectorAll("input");
+let btn = document.querySelector("button");
+let outputs = document.querySelectorAll(".outputs span");
+btn.addEventListener("click", calculateData);
+inputs.forEach((i)=>{
+    i.addEventListener("blur",dataCheck);
+})
+function calculateData(){
+    let currentDate = new Date();
+    outputs[0].innerHTML=Number((currentDate.getFullYear())-(inputs[2].value));
+    if(inputs[1].value>(currentDate.getMonth()+1)){
+        outputs[1].innerHTML=Number((inputs[1].value)-(currentDate.getMonth()+1))
     }
-    else if(inputs[0].value!=''){
-        inputs[0].style.border="1px solid #000";
-        msg[0].innerHTML='';
-        label[0].style.color="rgb(116, 116, 118)";
+    else if(inputs[1].value<=currentDate.getMonth()+1){
+        outputs[1].innerHTML=Number((currentDate.getMonth()+1)-(inputs[1].value))
     }
-    if(inputs[1].value==''){
-        inputs[1].style.border="1px solid #ec0f0f";
-        msg[1].innerHTML=erro;
-        label[1].style.color="#ec0f0f";
+    if(inputs[0].value>(currentDate.getDate())){
+        outputs[2].innerHTML=Number((inputs[0].value)-(currentDate.getDate()))
     }
-    else if(inputs[1].value!=''){
-        inputs[1].style.border="1px solid #000";
-        msg[1].innerHTML='';
-        label[1].style.color="rgb(116, 116, 118)";
-    }
-    if(inputs[2].value==''){
-        inputs[2].style.border="1px solid #ec0f0f";
-        msg[2].innerHTML=erro;
-        label[2].style.color="#ec0f0f";
-    }
-    else if(inputs[2].value!=''){
-        inputs[2].style.border="1px solid #000";
-        msg[2].innerHTML='';
-        label[2].style.color="rgb(116, 116, 118)";
+    else if(inputs[0].value<=currentDate.getDate()){
+        outputs[2].innerHTML=Number((currentDate.getDate())-(inputs[0].value))
     }
 }
-//FUNÇÃO CALCULAR DIA, MES E ANO DE VIDA
-function CalcAge(){
-    if(inputs[0].value==''||inputs[1].value==''||inputs[2].value==''){
-        dados();
+/*function: responsively for check each input individualy*/
+function dataCheck(inputTarg){
+    if(inputTarg.target.value==""||inputTarg.target.value<=0){
+        inputTarg.target.nextElementSibling.style.display="inline";
     }
-    else{
-    let day = parseInt(inputs[0].value);
-    let month = parseInt(inputs[1].value);
-    let year = parseInt(inputs[2].value);
-    let currentFullDate = new Date();
-    let currentDay = currentFullDate.getDate();
-    let currentMonth = currentFullDate.getMonth()+1;
-    let currentYear = currentFullDate.getFullYear();
-    //CALCULO DO DADO FORNECIDO PELO DADO DA DATA ATUAL
-    outputs[0].innerHTML=currentYear-year;
-    outputs[1].innerHTML=currentMonth-month;
-    outputs[2].innerHTML=currentDay-day;
-    //FORMATAÇÃO DA SAÍDA DE DADOS
-    outputs[0].innerHTML<0?outputs[0].innerHTML=0:outputs[0].innerHTML=currentYear-year;
-    outputs[1].innerHTML<0?outputs[1].innerHTML=0:outputs[1].innerHTML=currentMonth-month;
-    outputs[2].innerHTML<0?outputs[2].innerHTML=0:outputs[2].innerHTML=currentDay-day;
+    else if(inputTarg.target.value!=""||inputTarg.target.value>0){
+        inputTarg.target.nextElementSibling.style.display="none"; 
     }
 }
--- readme.md
+++ readme.md
@@ -1,1 +1,11 @@
-preciso adicionar labels na parte de baixo do input que vão aparecer quando o campo estiver errado, e preciso criar o script
+# age calculator app
+
+## code operation
+
+- there are 3 variables declared, *inputs*, *btn*, *outputs*;
+
+- respectively responsible for : (contains all input tag elements), (its a button to action), (contains all spans elements for output data)
+
+- **function dataCheck()** : responsible for check if the data provides its correctly, if not, show a message error getting and changing the *errMessage* to a display flex (previously in a display hidden)
+
+- **function calculateData()** : responsible for calculate the data provides with the data get for a variable housinh a object "new Date()"
